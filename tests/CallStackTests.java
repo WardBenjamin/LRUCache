f7920949d15525ddff9c3fcbb6654d4351a5ee0f
Ankur Gupta <rukna1000@gmail.com>
2017-11-05 18:54:53 -0500

    Added a local DataSource for testing purpoises, and created a couple of init methods

--- tests/CallStackTests.java
+++ tests/CallStackTests.java
@@ -1,2 +1,36 @@
+import org.junit.Before;
+import org.junit.Test;
+import java.util.HashMap;
+
 public class CallStackTests {
+    private class DataSource<T, U> implements DataProvider<T, U> {
+        private HashMap<T, U> _data;
+
+        public DataSource() {
+            _data = new HashMap<>();
+        }
+
+        public void insert(T key, U value) {
+            _data.put(key, value);
+        }
+
+        public U get(T key) {
+            return _data.get(key);
+        }
+    }
+
+    private DataProvider _dp1;
+
+    @Before
+    public void init() {
+        initDp1();
+    }
+
+    private void initDp1() {
+    }
+
+    @Test
+    public void test1() {
+
+    }
 }
